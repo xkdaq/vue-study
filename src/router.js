60393b591290b4d7f3b9d09cf91759ab4c2eae50
xukee <oncwnuBDYPJsFlWf_CezM2n3uqDU@git.weixin.qq.com>
2020-07-27 15:48:22 +0800

    add file

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from './components/Login.vue'
+
+Vue.use(Router)
+
+const router = new Router({
+    routes: [
+        { path: '/', redirect: '/login' },
+        { path: '/login', component: Login }
+    ]
+
+})
+
+export default router
